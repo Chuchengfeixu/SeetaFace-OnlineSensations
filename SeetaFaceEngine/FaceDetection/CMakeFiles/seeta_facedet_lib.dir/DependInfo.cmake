3dd1bb9592935046eef021437fdaad73230a7f3a
chuchengfeixu <chuchengfeixu@163.com>
2016-10-24 20:39:32 +0800

    add files

---
+++ SeetaFaceEngine/FaceDetection/CMakeFiles/seeta_facedet_lib.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dh/program/SeetaFaceEngine/FaceDetection/src/classifier/lab_boosted_classifier.cpp" "/home/dh/program/SeetaFaceEngine/FaceDetection/CMakeFiles/seeta_facedet_lib.dir/src/classifier/lab_boosted_classifier.cpp.o"
+  "/home/dh/program/SeetaFaceEngine/FaceDetection/src/classifier/mlp.cpp" "/home/dh/program/SeetaFaceEngine/FaceDetection/CMakeFiles/seeta_facedet_lib.dir/src/classifier/mlp.cpp.o"
+  "/home/dh/program/SeetaFaceEngine/FaceDetection/src/classifier/surf_mlp.cpp" "/home/dh/program/SeetaFaceEngine/FaceDetection/CMakeFiles/seeta_facedet_lib.dir/src/classifier/surf_mlp.cpp.o"
+  "/home/dh/program/SeetaFaceEngine/FaceDetection/src/face_detection.cpp" "/home/dh/program/SeetaFaceEngine/FaceDetection/CMakeFiles/seeta_facedet_lib.dir/src/face_detection.cpp.o"
+  "/home/dh/program/SeetaFaceEngine/FaceDetection/src/feat/lab_feature_map.cpp" "/home/dh/program/SeetaFaceEngine/FaceDetection/CMakeFiles/seeta_facedet_lib.dir/src/feat/lab_feature_map.cpp.o"
+  "/home/dh/program/SeetaFaceEngine/FaceDetection/src/feat/surf_feature_map.cpp" "/home/dh/program/SeetaFaceEngine/FaceDetection/CMakeFiles/seeta_facedet_lib.dir/src/feat/surf_feature_map.cpp.o"
+  "/home/dh/program/SeetaFaceEngine/FaceDetection/src/fust.cpp" "/home/dh/program/SeetaFaceEngine/FaceDetection/CMakeFiles/seeta_facedet_lib.dir/src/fust.cpp.o"
+  "/home/dh/program/SeetaFaceEngine/FaceDetection/src/io/lab_boost_model_reader.cpp" "/home/dh/program/SeetaFaceEngine/FaceDetection/CMakeFiles/seeta_facedet_lib.dir/src/io/lab_boost_model_reader.cpp.o"
+  "/home/dh/program/SeetaFaceEngine/FaceDetection/src/io/surf_mlp_model_reader.cpp" "/home/dh/program/SeetaFaceEngine/FaceDetection/CMakeFiles/seeta_facedet_lib.dir/src/io/surf_mlp_model_reader.cpp.o"
+  "/home/dh/program/SeetaFaceEngine/FaceDetection/src/util/image_pyramid.cpp" "/home/dh/program/SeetaFaceEngine/FaceDetection/CMakeFiles/seeta_facedet_lib.dir/src/util/image_pyramid.cpp.o"
+  "/home/dh/program/SeetaFaceEngine/FaceDetection/src/util/nms.cpp" "/home/dh/program/SeetaFaceEngine/FaceDetection/CMakeFiles/seeta_facedet_lib.dir/src/util/nms.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_OPENMP"
+  "USE_SSE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
